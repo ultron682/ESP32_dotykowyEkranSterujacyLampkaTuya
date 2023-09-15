5d6af8f66a17e0a4f049e814cba558eb462bc3b9
ultron682 <52131708+ultron682@users.noreply.github.com>
2023-09-15 16:14:56 +0200

    Update esp32_dht.ino

--- esp32_dht.ino
+++ esp32_dht.ino
@@ -6,7 +6,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-#include <FS.h>
+//#include <FS.h>
 #include "Free_Fonts.h" // Include the header file attached to this sketch
 
 #include <TFT_eSPI.h>    // Hardware-specific library
@@ -80,8 +80,8 @@ void setup()
   loopTask();
 
   // Calibrate the touch screen and retrieve the scaling factors
-  touch_calibrate();
-  initButtons();
+ // touch_calibrate();
+ // initButtons();
 }
 
 void loop()
@@ -113,186 +113,186 @@ void loop()
     // setCpuFrequencyMhz(40); // CPU
   }
 
-  static uint32_t scanTime = millis();
-  uint16_t t_x = 9999, t_y = 9999; // To store the touch coordinates
+  // static uint32_t scanTime = millis();
+  // uint16_t t_x = 9999, t_y = 9999; // To store the touch coordinates
 
-  // Scan keys every 50ms at most
-  if (millis() - scanTime >= 50)
-  {
-    // Pressed will be set true if there is a valid touch on the screen
-    bool pressed = tft.getTouch(&t_x, &t_y);
-    scanTime = millis();
-    for (uint8_t b = 0; b < buttonCount; b++)
-    {
-      if (pressed)
-      {
-        if (btn[b]->contains(t_x, t_y))
-        {
-          btn[b]->press(true);
-          btn[b]->pressAction();
-        }
-      }
-      else
-      {
-        btn[b]->press(false);
-        btn[b]->releaseAction();
-      }
-    }
-  }
+  // // Scan keys every 50ms at most
+  // if (millis() - scanTime >= 50)
+  // {
+  //   // Pressed will be set true if there is a valid touch on the screen
+  //   bool pressed = tft.getTouch(&t_x, &t_y);
+  //   scanTime = millis();
+  //   for (uint8_t b = 0; b < buttonCount; b++)
+  //   {
+  //     if (pressed)
+  //     {
+  //       if (btn[b]->contains(t_x, t_y))
+  //       {
+  //         btn[b]->press(true);
+  //         btn[b]->pressAction();
+  //       }
+  //     }
+  //     else
+  //     {
+  //       btn[b]->press(false);
+  //       btn[b]->releaseAction();
+  //     }
+  //   }
+  // }
 }
 
-void btnL_pressAction(void)
-{
-  if (btnL.justPressed())
-  {
-    Serial.println("Left button just pressed");
-    btnL.drawSmoothButton(true);
+// void btnL_pressAction(void)
+// {
+//   if (btnL.justPressed())
+//   {
+//     Serial.println("Left button just pressed");
+//     btnL.drawSmoothButton(true);
 
-    turnOnLEDS();
-  }
-}
+//     turnOnLEDS();
+//   }
+// }
 
-void btnL_releaseAction(void)
-{
-  static uint32_t waitTime = 1000;
-  if (btnL.justReleased())
-  {
-    Serial.println("Left button just released");
-    btnL.drawSmoothButton(false);
-    btnL.setReleaseTime(millis());
-    waitTime = 10000;
-  }
-  else
-  {
-    if (millis() - btnL.getReleaseTime() >= waitTime)
-    {
-      waitTime = 1000;
-      btnL.setReleaseTime(millis());
-      btnL.drawSmoothButton(btnL.getState());
-    }
-  }
-}
+// void btnL_releaseAction(void)
+// {
+//   static uint32_t waitTime = 1000;
+//   if (btnL.justReleased())
+//   {
+//     Serial.println("Left button just released");
+//     btnL.drawSmoothButton(false);
+//     btnL.setReleaseTime(millis());
+//     waitTime = 10000;
+//   }
+//   else
+//   {
+//     if (millis() - btnL.getReleaseTime() >= waitTime)
+//     {
+//       waitTime = 1000;
+//       btnL.setReleaseTime(millis());
+//       btnL.drawSmoothButton(btnL.getState());
+//     }
+//   }
+// }
 
-void btnR_pressAction(void)
-{
-  if (btnR.justPressed())
-  {
-    Serial.println("Left button just pressed");
-    btnR.drawSmoothButton(true);
+// void btnR_pressAction(void)
+// {
+//   if (btnR.justPressed())
+//   {
+//     Serial.println("Left button just pressed");
+//     btnR.drawSmoothButton(true);
 
-    turnOffLEDS();
-  }
-}
+//     turnOffLEDS();
+//   }
+// }
 
-void btnR_releaseAction(void)
-{
-  static uint32_t waitTime = 1000;
-  if (btnR.justReleased())
-  {
-    Serial.println("Right button just released");
-    btnR.drawSmoothButton(false);
-    btnR.setReleaseTime(millis());
-    waitTime = 10000;
-  }
-  else
-  {
-    if (millis() - btnR.getReleaseTime() >= waitTime)
-    {
-      waitTime = 1000;
-      btnR.setReleaseTime(millis());
-      btnR.drawSmoothButton(btnR.getState());
-    }
-  }
-}
+// void btnR_releaseAction(void)
+// {
+//   static uint32_t waitTime = 1000;
+//   if (btnR.justReleased())
+//   {
+//     Serial.println("Right button just released");
+//     btnR.drawSmoothButton(false);
+//     btnR.setReleaseTime(millis());
+//     waitTime = 10000;
+//   }
+//   else
+//   {
+//     if (millis() - btnR.getReleaseTime() >= waitTime)
+//     {
+//       waitTime = 1000;
+//       btnR.setReleaseTime(millis());
+//       btnR.drawSmoothButton(btnR.getState());
+//     }
+//   }
+// }
 
-void initButtons()
-{
-  uint16_t x = (tft.width() - BUTTON_W) - 3;
-  uint16_t y = 3;
-  btnL.initButtonUL(x, y, BUTTON_W, BUTTON_H, TFT_WHITE, TFT_GREEN, TFT_WHITE, "", 1);
-  btnL.setPressAction(btnL_pressAction);
-  btnL.setReleaseAction(btnL_releaseAction);
-  btnL.drawSmoothButton(false, 2, TFT_BLACK); // 3 is outline width, TFT_BLACK is the surrounding background colour for anti-aliasing
+// void initButtons()
+// {
+//   uint16_t x = (tft.width() - BUTTON_W) - 3;
+//   uint16_t y = 3;
+//   btnL.initButtonUL(x, y, BUTTON_W, BUTTON_H, TFT_WHITE, TFT_GREEN, TFT_WHITE, "", 1);
+//   btnL.setPressAction(btnL_pressAction);
+//   btnL.setReleaseAction(btnL_releaseAction);
+//   btnL.drawSmoothButton(false, 2, TFT_BLACK); // 3 is outline width, TFT_BLACK is the surrounding background colour for anti-aliasing
 
-  y = BUTTON_H + 6;
-  btnR.initButtonUL(x, y, BUTTON_W, BUTTON_H, TFT_WHITE, TFT_RED, TFT_WHITE, "", 1);
-  btnR.setPressAction(btnR_pressAction);
-  btnR.setReleaseAction(btnR_releaseAction);
-  btnR.drawSmoothButton(false, 2, TFT_BLACK); // 3 is outline width, TFT_BLACK is the surrounding background colour for anti-aliasing
-}
+//   y = BUTTON_H + 6;
+//   btnR.initButtonUL(x, y, BUTTON_W, BUTTON_H, TFT_WHITE, TFT_RED, TFT_WHITE, "", 1);
+//   btnR.setPressAction(btnR_pressAction);
+//   btnR.setReleaseAction(btnR_releaseAction);
+//   btnR.drawSmoothButton(false, 2, TFT_BLACK); // 3 is outline width, TFT_BLACK is the surrounding background colour for anti-aliasing
+// }
 
-void touch_calibrate()
-{
-  uint16_t calData[5];
-  uint8_t calDataOK = 0;
+// void touch_calibrate()
+// {
+//   uint16_t calData[5];
+//   uint8_t calDataOK = 0;
 
-  // check file system exists
-  if (!LittleFS.begin())
-  {
-    Serial.println("Formating file system");
-    LittleFS.format();
-    LittleFS.begin();
-  }
+//   // check file system exists
+//   if (!LittleFS.begin())
+//   {
+//     Serial.println("Formating file system");
+//     LittleFS.format();
+//     LittleFS.begin();
+//   }
 
-  // check if calibration file exists and size is correct
-  if (LittleFS.exists(CALIBRATION_FILE))
-  {
-    if (REPEAT_CAL)
-    {
-      // Delete if we want to re-calibrate
-      LittleFS.remove(CALIBRATION_FILE);
-    }
-    else
-    {
-      File f = LittleFS.open(CALIBRATION_FILE, "r");
-      if (f)
-      {
-        if (f.readBytes((char *)calData, 14) == 14)
-          calDataOK = 1;
-        f.close();
-      }
-    }
-  }
+//   // check if calibration file exists and size is correct
+//   if (LittleFS.exists(CALIBRATION_FILE))
+//   {
+//     if (REPEAT_CAL)
+//     {
+//       // Delete if we want to re-calibrate
+//       LittleFS.remove(CALIBRATION_FILE);
+//     }
+//     else
+//     {
+//       File f = LittleFS.open(CALIBRATION_FILE, "r");
+//       if (f)
+//       {
+//         if (f.readBytes((char *)calData, 14) == 14)
+//           calDataOK = 1;
+//         f.close();
+//       }
+//     }
+//   }
 
-  if (calDataOK && !REPEAT_CAL)
-  {
-    // calibration data valid
-    tft.setTouch(calData);
-  }
-  else
-  {
-    // data not valid so recalibrate
-    tft.fillScreen(TFT_BLACK);
-    tft.setCursor(20, 0);
-    tft.setTextFont(2);
-    tft.setTextSize(1);
-    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+//   if (calDataOK && !REPEAT_CAL)
+//   {
+//     // calibration data valid
+//     tft.setTouch(calData);
+//   }
+//   else
+//   {
+//     // data not valid so recalibrate
+//     tft.fillScreen(TFT_BLACK);
+//     tft.setCursor(20, 0);
+//     tft.setTextFont(2);
+//     tft.setTextSize(1);
+//     tft.setTextColor(TFT_WHITE, TFT_BLACK);
 
-    tft.println("Touch corners as indicated");
+//     tft.println("Touch corners as indicated");
 
-    tft.setTextFont(1);
-    tft.println();
+//     tft.setTextFont(1);
+//     tft.println();
 
-    if (REPEAT_CAL)
-    {
-      tft.setTextColor(TFT_RED, TFT_BLACK);
-      tft.println("Set REPEAT_CAL to false to stop this running again!");
-    }
+//     if (REPEAT_CAL)
+//     {
+//       tft.setTextColor(TFT_RED, TFT_BLACK);
+//       tft.println("Set REPEAT_CAL to false to stop this running again!");
+//     }
 
-    tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
+//     tft.calibrateTouch(calData, TFT_MAGENTA, TFT_BLACK, 15);
 
-    tft.setTextColor(TFT_GREEN, TFT_BLACK);
-    tft.println("Calibration complete!");
+//     tft.setTextColor(TFT_GREEN, TFT_BLACK);
+//     tft.println("Calibration complete!");
 
-    // store data
-    File f = LittleFS.open(CALIBRATION_FILE, "w");
-    if (f)
-    {
-      f.write((const unsigned char *)calData, 14);
-      f.close();
-    }
-  }
-}
+//     // store data
+//     File f = LittleFS.open(CALIBRATION_FILE, "w");
+//     if (f)
+//     {
+//       f.write((const unsigned char *)calData, 14);
+//       f.close();
+//     }
+//   }
+// }
 
 void onWiFiEvent(WiFiEvent_t event)
 {
